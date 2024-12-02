63bd8b5dbf4f1a839a396a3cb3487ddc9776f88f
MateyGetRekt <95328019+MateyGetRekt@users.noreply.github.com>
2024-12-02 12:52:42 +0800

    Update daxpy.c

--- Cmain_program/Cmain_program/daxpy.c
+++ Cmain_program/Cmain_program/daxpy.c
@@ -4,21 +4,15 @@
 
 extern void daxpy_asm(long n, double A, double* X, double* Y, double* Z);
 
-void daxpy_c(long n, double A, double* X, double* Y, double* Z) {
-    for (int i = 0; i < n; i++) {
-        Z[i] = A * X[i] + Y[i];
-    }
-}
-
 int main() {
-    //CLOCK 
+
     clock_t start, end;
     double time_for_C;
 
     int n = 1 << 28; 
 
-    double A = (double)(rand() % 1000) / 10.0; 
-    printf("%.2f\n", A);
+    double A = (double)(rand() % 1000) / 10.0;  
+    printf("Scalar A: %.2f\n", A);
 
     srand(time(NULL));
 
@@ -27,22 +21,22 @@ int main() {
     double* Z = (double*)malloc(n * sizeof(double));
 
     for (long i = 0; i < n; i++) {
-        X[i] = (double)(rand() % 1000) / 10.0; 
+        X[i] = (double)(rand() % 1000) / 10.0;
         Y[i] = (double)(rand() % 1000) / 10.0;
     }
 
-    //CLOCK
     start = clock();
-    daxpy_asm(n, A, X, Y, Z); 
+    daxpy_asm(n, A, X, Y, Z);
     end = clock();
+
     time_for_C = ((double)(end - start)) / CLOCKS_PER_SEC * 1000;
 
-    printf("\nOUTPUT Z: ");
+    printf("\nOUTPUT Z (first 10 values): ");
     for (int i = 0; i < 10; i++) {
         printf("%.2f ", Z[i]);
     }
     printf("\n");
-    printf("C time: %f ms\n", time_for_C);
+    printf("Assembly execution time: %f ms\n", time_for_C);
 
     free(X);
     free(Y);
