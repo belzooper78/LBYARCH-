fd22c09fb38e8501e9ef48f8860487035ed0d41b
MateyGetRekt <95328019+MateyGetRekt@users.noreply.github.com>
2024-12-02 12:52:18 +0800

    Update asmfunc.asm

--- Cmain_program/Cmain_program/asmfunc.asm
+++ Cmain_program/Cmain_program/asmfunc.asm
@@ -1,54 +1,41 @@
 section .data
-msg db "Hello World", 13, 10, 0 ; message for testing p
-
 section .text
 bits 64
 default rel
-
-global asmhello
 global daxpy_asm
-extern printf
 
-; asmhello function (for testing purposes)
-asmhello:
-    sub rsp, 8*5           
-    lea rcx, [msg]        
-    call printf            
-    add rsp, 8*5           
-    ret
 
 daxpy_asm:
-    push rbp
-    mov rbp, rsp
-    push rbx
-    push r12
+    push rbx                
+    push r12                
 
-    mov rdi, rdx        
-    mov rsi, rcx        
-    mov rdx, r8         
-    mov r8, r9          
-    mov r9, r10        
+    sub rsp, 8             
 
-    xor rbx, rbx          
+    mov rdi, rdx            
+    mov rsi, rcx            
+    mov rdx, r8            
+    mov r8, r9             
+    mov r9, r10             
+
+    xor rbx, rbx           
 
 loop:
     cmp rbx, rdi           
-    jge .end_loop           
+    jge .end_loop          
 
     movsd xmm0, [rdx + rbx * 8] 
     movsd xmm1, [r8 + rbx * 8]  
 
-    mulsd xmm0, rsi        
-    addsd xmm0, xmm1      
+    mulsd xmm0, rsi         
+    addsd xmm0, xmm1        
 
-    movsd [r9 + rbx * 8], xmm0 ; Store result in Z[i]
+    movsd [r9 + rbx * 8], xmm0 
 
     inc rbx
     jmp loop
 
 end_loop:
+    add rsp, 8             
     pop r12
     pop rbx
-    mov rsp, rbp
-    pop rbp
     ret
